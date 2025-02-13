d525b71fcba4db74673aa06650c2629c895c297a
its-topdev <neil.zhao.adams@gmail.com>
2025-02-13 10:15:16 -0600

    Added health check API to test database connection

--- routs/config_route.js
+++ routs/config_route.js
@@ -1,4 +1,4 @@
-require('express-async-errors');
+require("express-async-errors");
 
 const indexR = require("./index");
 const generalR = require("./general");
@@ -12,26 +12,37 @@ const chargeTypeR = require("./charge_type");
 const countryR = require("./country");
 const shipperR = require("./shipper");
 const carrierR = require("./carrier");
+const healthR = require("./health");
 
 exports.routeInit = (app) => {
-     app.use("/", indexR);
-     app.use("/general", generalR);
-     app.use("/user", userR);
-     app.use("/port", portR);
-     app.use("/price", priceR);
-     app.use("/vessel", vesselR);
-     app.use("/book", bookRequestR);
-     app.use("/charge", chargeR);
-     app.use("/charge-type", chargeTypeR);
-     app.use("/country", countryR);
-     app.use("/shipper", shipperR);
-     app.use("/carrier", carrierR);
+  app.use("/", indexR);
+  app.use("/general", generalR);
+  app.use("/user", userR);
+  app.use("/port", portR);
+  app.use("/price", priceR);
+  app.use("/vessel", vesselR);
+  app.use("/book", bookRequestR);
+  app.use("/charge", chargeR);
+  app.use("/charge-type", chargeTypeR);
+  app.use("/country", countryR);
+  app.use("/shipper", shipperR);
+  app.use("/carrier", carrierR);
+  app.use("/health", healthR);
 
-     app.use((err, req, res, next) => {
-          console.log("Global Error:" + req.url+ " " + err.stack + "\n Body: " + JSON.stringify(req.body) + "\n Params: " + JSON.stringify(req.params));
-          return res.json({
-              status: false,
-              clientErrMsg: 'error...'
-          });  
-     });
-}
+  app.use((err, req, res, next) => {
+    console.log(
+      "Global Error:" +
+        req.url +
+        " " +
+        err.stack +
+        "\n Body: " +
+        JSON.stringify(req.body) +
+        "\n Params: " +
+        JSON.stringify(req.params)
+    );
+    return res.json({
+      status: false,
+      clientErrMsg: "error...",
+    });
+  });
+};
---
+++ routs/health.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { sequelize } = require("../models");
+
+router.get("/health", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.status(200).json({
+      status: "OK",
+      message: "Database connection successful",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "ERROR",
+      message: "Database connection failed",
+    });
+  }
+});
+
+module.exports = router;
