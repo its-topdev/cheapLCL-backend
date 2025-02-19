c1e3e46638f4497fea80975f189af266a3192382
its-topdev <neil.zhao.adams@gmail.com>
2025-02-18 18:14:29 -0600

    Temp: for vercel1

--- app.js
+++ app.js
@@ -7,23 +7,26 @@ const http = require("http");
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
-const { sequelize } = require("./models");
+// const { sequelize } = require("./models");
 
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("DB connected");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// sequelize
+//   .sync({ force: false })
+//   .then(() => {
+//     console.log("DB connected");
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
-const { routeInit } = require("./routs/config_route");
+// const { routeInit } = require("./routs/config_route");
 const port = process.env.port || 3001;
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-routeInit(app);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+//   routeInit(app);
 const server = http.createServer(app);
 server.listen(port, () => console.log("Server running on port " + port));
