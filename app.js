816c66f4cc528ea058a1f25896fb5f27750daf5d
its-topdev <neil.zhao.adams@gmail.com>
2025-02-18 18:19:13 -0600

    Vercel: added sequelize in app

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const http = require("http");
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
-// const { sequelize } = require("./models");
+const { sequelize } = require("./models");
 
-// sequelize
-//   .sync({ force: false })
-//   .then(() => {
-//     console.log("DB connected");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const { routeInit } = require("./routs/config_route");
 const port = process.env.port || 3001;
