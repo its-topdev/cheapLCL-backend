a1fddca1ebf874b17abeb068f7c75f718b80a925
its-topdev <neil.zhao.adams@gmail.com>
2025-03-12 05:43:30 -0700

    Added prices applicable timeframe table

---
+++ migrations/20250312121129-create_prices_applicable_timeframes_table.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("prices_applicable_timeframes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      prices_start_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      prices_end_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("prices_applicable_timeframes");
+  },
+};
---
+++ migrations/20250312121401-add_applicable_timeframe_id_to_prices_table.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("prices", "applicable_timeframe_id", {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: "prices_applicable_timeframes",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("prices", "applicable_timeframe_id");
+  },
+};
