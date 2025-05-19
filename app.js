b349fc2b6e1ff5e01ca203a3b68b064faa67b496
its-topdev <neil.zhao.adams@gmail.com>
2025-05-19 18:28:45 +0800

    reset the frontend URL

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const { sequelize } = require('./models');
 const app = express();
 const port = process.env.PORT || 3001;
 const allowedOrigins = [
-  'https://www.cheaplclfrontend.com',
-  'http://www.cheaplclfrontend.com',
-  'http://cheaplclfrontend.com',
-  'https://cheaplclfrontend.com',
+  'https://www.cheaplclfrontend.vercel.app',
+  'http://www.cheaplclfrontend.vercel.app',
+  'http://cheaplclfrontend.vercel.app',
+  'https://cheaplclfrontend.vercel.app',
   'http://localhost:5173',
   'https://cheap-lcl-backend.onrender.com',
 ];
